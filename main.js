9df1f05b356a7925e30b9b325ccb327653534143
josetabuyo <josetabuyo@gmail.com>
2015-03-06 09:11:45 -0300

    JT: commit inicial

---
+++ main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {  
+    //toda esta garcha es para detectar si la aplicacion esta corriendo en un celular o en una pc.
+    //En el celular para arrancar la app hay que esperar al evento deviceReady, en la pc solo al documentReady
+    //window.isphone = false;
+	window.isphone = (document.URL.indexOf("com.") > 0);
+
+    if(window.isphone) {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    } else {
+        onDeviceReady();
+    }
+});
+
+
+var onDeviceReady = function() {
+    
+	vx.start({verbose:true});
+    
+	vx.conectarPorWebSockets({
+		url: "https://backend-sime.herokuapp.com"
+	});
+	
+	
+	
+	$('#enviarMedicion').on('click', function(){
+		console.log('e paso?');
+		vx.send({
+			tipoDeMensaje: 'medicionCruda',
+			idAdaptador: 1,
+			valorMedicion: $('#enviarMedicion').val()
+		});
+		
+	});
+	
+	
+	
+	if(window.plugin){
+		window.plugin.backgroundMode.enable();
+	}
+};
+
+
+
+
